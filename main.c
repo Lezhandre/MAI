a2802b2db9eb64849fa637707dafa3c89eb44a83
Lezhandre <50102924+Lezhandre@users.noreply.github.com>
2020-09-20 20:46:46 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,10 @@
-//
-//  main.c
-//  прогание наяву
-//
-//  Created by Michael Duzhenko on 10/09/2020.
-//  Copyright © 2020 Michael Duzhenko. All rights reserved.
-//
+#include <stdio.h>
+#include <locale.h>
 
-/*\
- #include <stdio.h>
-
+/*\*/
 void comrade(FILE*);
 
-int main(int argc, const char * argv[]) {
+int main() {
     FILE* trash = fopen("/Users/michaelduzhenko/Documents/Code on XCode/прогание наяву/прогание наяву/a.txt", "r");
     comrade(trash);
     return 0;
@@ -21,7 +14,7 @@ void comrade(FILE* file){
     if (file != NULL) {
         long long sum;
         unsigned i = 0;
-        char c = 1, f;
+        int c = 1, f;
         while (c != EOF){
             ++i;
             sum = 0;
@@ -29,20 +22,19 @@ void comrade(FILE* file){
             while ((c = getc(file)) != EOF && c != '\n'){
                 sum += c * f;
                 f = -f;
+                printf("%c", c);
             }
-            printf("Контрольная сумма %d-ой строки: %Ld\n", i, sum);
+            printf("\nКонтрольная сумма %d-ой строки: %lld\n", i, sum);
         }
     }
     else
-        printf("Файл не найден\n");
+        printf("Файл не открылся\n");
 }
-\*/
+/*\*/
 
-
-#include <stdio.h>
-#include <stdlib.h>
-
+/*\
 int main() {
+    setlocale(LC_ALL, "Russian_Russia.1251");
     FILE *out = fopen("/Users/michaelduzhenko/Documents/Code on XCode/прогание наяву/прогание наяву/a.txt", "r");
     FILE *in = fopen("/Users/michaelduzhenko/Documents/Code on XCode/прогание наяву/a.bin", "wb");
     u_char str[257];
@@ -52,28 +44,29 @@ int main() {
             int sum = 0;
             unsigned char num_of_let = 0;
             for (i; str[i] != '\n' && str[i] != '\0'; ++i){
-                if (str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z')
+                if ((str[i] >= 'a' && str[i] <= 'z') || str[i] >= 'A' && str[i] <= 'Z' ||
+                    str[i] >= '\xe0' && str[i] <= '\xff' || str[i] >= '\xco' && str[i] <= '\xdf')
                     ++num_of_let;
                 sum += str[i];
             }
             short f = fwrite(&(i), sizeof(char), 1, in);
             if (f < 0) {
-                printf("Произошла ошибка сувания инфы в бинарник! Остановка жеппы!");
+                printf("Неправильно присунули в бинарник!");
                 return 0;
             }
             f = fwrite(&(num_of_let), sizeof(char), 1, in);
             if (f < 0) {
-                printf("Произошла ошибка сувания инфы в бинарник! Остановка жеппы!");
+                printf("Неправильно присунули в бинарник!");
                 return 0;
             }
             f = fwrite(&(sum), sizeof(int), 1, in);
             if (f < 0) {
-                printf("Произошла ошибка сувания инфы в бинарник! Остановка жеппы!");
+                printf("Неправильно присунули в бинарник!");
                 return 0;
             }
             f = fwrite(str, i, 1, in);
             if (f < 0) {
-                printf("Произошла ошибка сувания инфы в бинарник! Остановка жеппы!");
+                printf("Неправильно присунули в бинарник!");
                 return 0;
             }
         }
@@ -82,22 +75,15 @@ int main() {
     fclose(out);
     out = fopen("/Users/michaelduzhenko/Documents/Code on XCode/прогание наяву/a.bin", "rb");
     size_t n_obj;
-    /*while((n_obj = fread(str, sizeof(char), 40, out)) != 0){
-        for (unsigned i = 0; i < n_obj; ++i){
-            if (16 > str[i])
-                printf("0");
-            printf("%X", str[i]);
-        }
-        printf("\n");
-    }*/
-    unsigned size;
-    while((n_obj = fread(&size, sizeof(char), 1, out)) != 0){
-        fread(str, size + sizeof(char) + sizeof(int), 1, out);
-        for (int i = 0; i < size + sizeof(char) + sizeof(int); ++i)
-            printf("%c", str[i]);
-        printf("\n");
+    while((n_obj = fread(str, sizeof(char), 40, out)) != 0){
+    for (unsigned i = 0; i < n_obj; ++i){
+        if (16 > str[i])
+            printf("0");
+        printf("%X", str[i]);
+    }
+    printf("\n");
     }
     fclose(out);
     return 0;
 }
-
+/*\*/
