4af9736dfe9ea264b4c1f3bfbaba23e1f2f36911
DMA <mdauzhenko@macbook-pro-dma.local>
2021-04-03 11:08:17 +0300

    Initial Commit

---
+++ NumMeth_HW/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NumMeth_HW
+//
+//  Created by DMA on 03.04.2021.
+//  Copyright © 2021 DMA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
